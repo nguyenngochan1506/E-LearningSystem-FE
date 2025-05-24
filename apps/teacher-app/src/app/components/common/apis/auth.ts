2644d656d7a40d5856807843db0f8354d6fc835d
TuyetNhungHihi <22130200@st.hcmuaf.edu.vn>
2025-05-24 19:16:41 +0700

    connect backend login + quên mật khẩu

--- apps/teacher-app/src/app/components/common/apis/auth.ts
+++ apps/teacher-app/src/app/components/common/apis/auth.ts
@@ -1,13 +1,13 @@
 const API_URL = import.meta.env.VITE_API_URL;
-
+const LANGUAGE = localStorage.getItem('language');
 export const login = async (email: string, password: string) => {
-    const language = localStorage.getItem('language');
+    
     const response = await fetch(`${API_URL}/auth/login`, {
       method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'Accept-Language': language ,
+            'Accept-Language': LANGUAGE ,
         },
         body: JSON.stringify({
             email,
@@ -26,3 +26,45 @@ export const login = async (email: string, password: string) => {
     const data = await response.json();
     return data.data; // Trả về dữ liệu (ví dụ: token)
 };
+
+export const sendEmailForgotPassword = async (email: string) => {
+    const response = await fetch(`${API_URL}/auth/forgot-password`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Accept-Language': LANGUAGE
+        },
+        body: JSON.stringify({ email }),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+    }
+
+    const data = await response.json();
+    
+    return data; // Trả về dữ liệu nếu cần
+}
+
+export const resetPassword = async (secretKey: string, newPassword: string) => {
+    const response = await fetch(`${API_URL}/auth/reset-password`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Accept-Language': LANGUAGE
+        },
+        body: JSON.stringify({ secretKey, newPassword }),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+    }
+
+    const data = await response.json();
+    
+    return data; // Trả về dữ liệu nếu cần
+}
